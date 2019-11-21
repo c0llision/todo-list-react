c43fb3423e03bdb748a7adf66353a67490816aac
c0llision <c0llision@protonmail.com>
2019-11-21 14:08:20 +0000

    open webpage in browser when server started

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
+console.log(`
+ _____         _         _     _     _
+|_   _|__   __| | ___   | |   (_)___| |_
+  | |/ _ \\ / _\` |/ _ \\  | |   | / __| __|
+  | | (_) | (_| | (_) | | |___| \\__ \\ |_
+  |_|\\___/ \\__,_|\\___/  |_____|_|___/\\__|
+`);
+
+
 const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = 4000
 
-app.use(express.urlencoded())
+const url = `http://localhost:3000/`
+var opn = require('opn');
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cors());
 
@@ -17,12 +29,8 @@ app.use(function(req, res, next) {
 app.get('/', (req, res) => res.send('Welcome to Data Representation & Querying'))
 
 
-console.log(`
- _____         _         _     _     _
-|_   _|__   __| | ___   | |   (_)___| |_
-  | |/ _ \\ / _\` |/ _ \\  | |   | / __| __|
-  | | (_) | (_| | (_) | | |___| \\__ \\ |_
-  |_|\\___/ \\__,_|\\___/  |_____|_|___/\\__|
-      `);
+app.listen(port, () => {
+    console.log(`Todo List backend server listening on ${url}`)
+    opn(url);
+})
 
-app.listen(port, () => console.log(`Todo List backend server listening on http://localhost:${port}/`))
