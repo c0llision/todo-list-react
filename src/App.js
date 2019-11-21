a3e86f2446ade109457593b0c6d667ad0782c6e1
c0llision <c0llision@protonmail.com>
2019-11-21 13:14:09 +0000

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Todo List</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/add-task">Add Task</Nav.Link>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
