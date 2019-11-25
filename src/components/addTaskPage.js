0f5468baba0ffc553255d080a0dd6e5b0580827a
c0llision <c0llision@protonmail.com>
2019-11-25 13:21:18 +0000

    add new task form

--- src/components/addTaskPage.js
+++ src/components/addTaskPage.js
@@ -1,10 +1,65 @@
 import React from 'react';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
 class AddTaskPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            taskName: '',
+            dueDate: new Date()
+        };
+
+        this.handleTaskName = this.handleTaskName.bind(this);
+        this.handleDueDate = this.handleDueDate.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleTaskName(event) {
+        this.setState({taskName: event.target.value});
+    }
+
+    handleDueDate(date) {
+        this.setState({dueDate: date});
+    }
+
+    handleSubmit(event) {
+        alert(this.state.taskName);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="App">
-                Add task page
+                <br />
+                <h1>Add new task</h1>
+                <Form style={{width:500, margin:"auto"}} onSubmit={this.handleSubmit}>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="text" value={this.state.taskName} onChange={this.handleTaskName}  placeholder="Enter task name" />
+                    <Form.Text className="text-muted">
+                        Give your task a descriptive name
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Due date: </Form.Label>
+                     <DatePicker selected={this.state.dueDate} onChange={this.handleDueDate} />
+                    <Form.Text className="text-muted">
+                        This is optional
+                    </Form.Text>
+                  </Form.Group>
+                  <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                  </Form.Group>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Form>
             </div>
         );
     }
